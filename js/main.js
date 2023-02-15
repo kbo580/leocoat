8ce706216c6c45179b5f0c68fa441065554be7f8
kbo580 <kobayuta1025@gmail.com>
2023-02-15 11:37:50 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function(){
   $('#show').on('click', function(){
     $(this).toggleClass('active');
-    $('#nav').toggleClass('active');
-    $('body').toggleClass('active');
+    $('.header__bottom').toggleClass('active');
+    // $('body').toggleClass('active');
   });
   
   $('.nav-menu li a').on('click', function () {
       $('#show').removeClass('active');
       $('#nav').removeClass('active');
-      $('body').removeClass('active');
+      // $('body').removeClass('active');
   });
 });
