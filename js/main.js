4a12e85243c739554f8b6f964a131a61a95f3185
kbo580 <kobayuta1025@gmail.com>
2023-02-19 21:03:51 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -7,16 +7,29 @@ $(function(){
   });
   
   $('.nav-menu li a').on('click', function () {
-      $('#show').removeClass('active');
-      $('#nav').removeClass('active');
-      // $('body').removeClass('active');
+    $('#show').removeClass('active');
+    $('#nav').removeClass('active');
+    // $('body').removeClass('active');
   });
-  $('.doukonhin-btn').on('click', function () {
-      $(this).next('.doukonhin').slideToggle();
-      $(this).toggleClass("active");
 
-      return false;
+  $('.doukonhin-btn').on('click', function () {
+    $(this).next('.doukonhin').slideToggle();
+    $(this).toggleClass("active");
+
+    return false;
   });
+
+  $('.qaa__wrap').on('click', function () {
+    var answer = $(this).find(".answer__wrap");
+    answer.slideToggle(200);
+    if($(this).children('.question__wrap').hasClass('active')){
+      $(this).children('.question__wrap').removeClass('active');
+    }
+    else{
+      $(this).children('.question__wrap').addClass('active');
+    }
+  });
+
 
 
 });
