1618930295c3e4257412e81d4cf788c9d1770fca
kbo580 <kobayuta1025@gmail.com>
2023-02-20 18:55:05 +0900

    headerまでやったけど再確認すること

--- js/main.js
+++ js/main.js
@@ -19,14 +19,26 @@ $(function(){
     return false;
   });
 
-  $('.qaa__wrap').on('click', function () {
-    var answer = $(this).find(".answer__wrap");
+  $('.question').on('click', function () {
+    var answer = $(this).next(".answer");
     answer.slideToggle(200);
     if($(this).children('.question__wrap').hasClass('active')){
       $(this).children('.question__wrap').removeClass('active');
     }
     else{
       $(this).children('.question__wrap').addClass('active');
+    }
+  });
+
+  $(window).on('scroll', function(){
+    var headerHight =$('.header__wrap').outerHeight(true);
+    if($(this).scrollTop()>= headerHight){
+      $('.site-logo__image').addClass('scroll');
+      $('.header').addClass('scroll');
+    }
+    else{
+      $('.site-logo__image').removeClass('scroll');
+      $('.header').removeClass('scroll');
     }
   });
 
