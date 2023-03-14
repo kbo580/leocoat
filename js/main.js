8ab0b3e699d7dd55915358a2492d70679c8f6dca
kbo580 <kobayuta1025@gmail.com>
2023-03-14 21:32:54 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(function($){
 
 
 $(function(){
-
+  
   //----- バーガーメニュー -----
   $('#show').on('click', function(){
     $(this).toggleClass('active');
@@ -48,61 +48,40 @@ $(function(){
   });
 
   //----- instaのスライド -----
-  var instaSwiper=  new Swiper('.instaSwiper', {
-    slidesPerView: '1',
-    spaceBetween: 30,
-    breakpoints: {
-      768: {
-        slidesPerView: 3.5,
-        spaceBetween: 30,
-      }
-    },
-    grabCursor: true,
-    centeredSlides: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    loop: true,
-    // loopAdditionalSlides: 1,
-    autoplay: {
-      delay: 4000,
-      waitForTransition: true,
-      disableOnInteraction: false,
-    },
-    speed: 1000,
-    effect: 'slide',
-    fadeEffect: {
-      crossFade: true
-    },
-  });
-
-  //---- モーダル -----
-  // $(function(){
-  //   var open = $('.modal-open');
-  //   var close = $('.modal-close');
-  //   var container = $('#modal-container');
-
-  //   $(open).on('click', function(){
-  //     var src = $(this).children('img').attr('src');
-  //     var alt = $(this).children('img').attr('alt');
-  //     $('#modalImage').attr('src', src).attr('alt', alt);
-  //     $(container).addClass('active');
-  //     return false;
-  //   });
-    
-  //   $(close).on('click', function(){
-  //     $(container).removeClass('active');
-  //   });
-
-  //   $(container).on('click', function(){
-  //     $(container).removeClass('active');
-  //   });
-  // });
+  if ( window.document.body.id === 'top' ) {
+    var instaSwiper=  new Swiper('.instaSwiper', {
+      slidesPerView: '1',
+      spaceBetween: 30,
+      breakpoints: {
+        768: {
+          slidesPerView: 3.5,
+          spaceBetween: 30,
+        }
+      },
+      grabCursor: true,
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      loop: true,
+      // loopAdditionalSlides: 1,
+      autoplay: {
+        delay: 4000,
+        waitForTransition: true,
+        disableOnInteraction: false,
+      },
+      speed: 1000,
+      effect: 'slide',
+      fadeEffect: {
+        crossFade: true
+      },
+    });
+  }
 
   //----- タブメニュー-----
   $('.tab-title').on('click', function(){
